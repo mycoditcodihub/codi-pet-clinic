95350dfad40a61d134c4c4ea7f23c27def78e6e6
codihub <codibeka@gmail.com>
2018-08-05 12:38:33 -0600

    Initial Project Add

---
+++ src/main/java/com/codihub/codipetclinic/CodiPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.codihub.codipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodiPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodiPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/com/codihub/codipetclinic/CodiPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codihub.codipetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodiPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
