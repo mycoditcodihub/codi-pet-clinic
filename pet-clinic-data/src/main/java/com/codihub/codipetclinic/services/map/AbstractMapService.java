e72dc7b3493790cb3c745bfad03f899de4aad00a
codihub <codibeka@gmail.com>
2018-08-24 16:54:02 -0600

    Implement mapped based services. closes #6

---
+++ pet-clinic-data/src/main/java/com/codihub/codipetclinic/services/map/AbstractMapService.java
@@ -0,0 +1,33 @@
+package com.codihub.codipetclinic.services.map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> {
+
+    protected Map<ID, T> map = new HashMap<>();
+
+    Set<T> findAll() {
+        return new HashSet<>(map.values());
+    }
+
+    T findById(ID id) {
+        return map.get(id);
+    }
+
+    //Because we are using generics, we do not know what type of object we are using so we just use object of type T
+    T save(ID id, T object) {
+        map.put(id, object);
+        return object;
+    }
+
+    void deleteById(ID id) {
+        map.remove(id);
+    }
+
+    void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+}
