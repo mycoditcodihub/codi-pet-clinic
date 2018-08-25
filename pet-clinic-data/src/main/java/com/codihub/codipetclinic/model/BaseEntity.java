bd824f81e580cee90899602dde087a0d4e5a7503
codihub <codibeka@gmail.com>
2018-08-24 21:52:04 -0600

    Adding Vet index page and controller. Closes #7

--- pet-clinic-data/src/main/java/com/codihub/codipetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/codihub/codipetclinic/model/BaseEntity.java
@@ -2,6 +2,10 @@ package com.codihub.codipetclinic.model;
 
 import java.io.Serializable;
 
+/*
+ * This class is to provide all the other POJO classes with an id value. So instead of adding the id value in each POJO class,
+ * we just create one class with the id variable and all the POJO will extend this base entity class
+ */
 public class BaseEntity implements Serializable {
 
     //JPA recommends to user wrapper classes than primitives because for instance, you have have a null object
