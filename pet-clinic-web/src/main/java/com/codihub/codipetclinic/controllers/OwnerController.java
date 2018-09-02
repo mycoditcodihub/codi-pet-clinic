28cfaafc6ce2ff1e4b6887e5030e1225efd9da9b
codihub <codibeka@gmail.com>
2018-09-01 21:10:48 -0600

    adding owners list and updated controller. Closes #16

--- pet-clinic-web/src/main/java/com/codihub/codipetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/codihub/codipetclinic/controllers/OwnerController.java
@@ -1,15 +1,27 @@
 package com.codihub.codipetclinic.controllers;
 
+import com.codihub.codipetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //The advantage of having a RequestMapping added at the class level is that any request that comes in with /owner gets added to every RequestMapping in each method in this class
 @RequestMapping("/owners")
 @Controller
-public class PetController {
+public class OwnerController {
+
+    //We need to get an OwnerService object
+    private final OwnerService ownerService;
+
+    //Using constructor based
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
